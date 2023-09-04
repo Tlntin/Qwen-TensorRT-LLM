341f9604b93f6944c5f96b086b907c220a0f6620
Tlntin <TlntinDeng01@Gmail.com>
2023-09-04 07:42:35 +0000

    update summarize.py

--- tensorrt_llm_july-release-v1/examples/qwen/summarize.py
+++ tensorrt_llm_july-release-v1/examples/qwen/summarize.py
@@ -5,17 +5,27 @@ import os
 
 import numpy as np
 import torch
-from datasets import load_dataset, load_metric
-from transformers import AutoModelForCausalLM, LlamaTokenizer
+from datasets import load_dataset
+from evaluate import load
+from transformers import AutoModelForCausalLM, GenerationConfig
+# for debug
+# from qwen_7b_chat.tokenization_qwen import QWenTokenizer as AutoTokenizer
+# for release
+from transformers import AutoTokenizer
 
 import tensorrt_llm
 import tensorrt_llm.profiler as profiler
 from tensorrt_llm.logger import logger
+from run import QWenForCausalLMGenerationSession
+from utils import make_context
 
 from build import get_engine_name  # isort:skip
 
 
-def TRTLLaMA(args, config):
+now_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def TRT_QWen(args, config):
     dtype = config['builder_config']['precision']
     world_size = config['builder_config']['tensor_parallel']
     assert world_size == tensorrt_llm.mpi_world_size(), \
@@ -44,7 +54,7 @@ def TRTLLaMA(args, config):
     runtime_mapping = tensorrt_llm.Mapping(world_size, runtime_rank)
     torch.cuda.set_device(runtime_rank % runtime_mapping.gpus_per_node)
 
-    engine_name = get_engine_name('llama', dtype, world_size, runtime_rank)
+    engine_name = get_engine_name('qwen', dtype, world_size, runtime_rank)
     serialize_path = os.path.join(args.engine_dir, engine_name)
 
     tensorrt_llm.logger.set_level(args.log_level)
@@ -52,9 +62,11 @@ def TRTLLaMA(args, config):
     profiler.start('load tensorrt_llm engine')
     with open(serialize_path, 'rb') as f:
         engine_buffer = f.read()
-    decoder = tensorrt_llm.runtime.GenerationSession(model_config,
-                                                     engine_buffer,
-                                                     runtime_mapping)
+    decoder = QWenForCausalLMGenerationSession(
+        model_config,
+        engine_buffer,
+        runtime_mapping
+    )
     profiler.stop('load tensorrt_llm engine')
     tensorrt_llm.logger.info(
         f'Load engine takes: {profiler.elapsed_time_in_sec("load tensorrt_llm engine")} sec'
@@ -70,18 +82,28 @@ def main(args):
     test_trt_llm = args.test_trt_llm
     hf_model_location = args.hf_model_location
     profiler.start('load tokenizer')
-    tokenizer = LlamaTokenizer.from_pretrained(hf_model_location,
-                                               legacy=False,
-                                               padding_side='left')
+    tokenizer = AutoTokenizer.from_pretrained(
+        hf_model_location,
+        legacy=False,
+        padding_side='left',
+        trust_remote_code=True,
+    )
     profiler.stop('load tokenizer')
     tensorrt_llm.logger.info(
         f'Load tokenizer takes: {profiler.elapsed_time_in_sec("load tokenizer")} sec'
     )
     tokenizer.pad_token = tokenizer.eos_token
-
-    dataset_cnn = load_dataset("ccdv/cnn_dailymail",
-                               '3.0.0',
-                               cache_dir=args.dataset_path)
+    # load dataset from local or huggingface
+    if os.path.exists(args.dataset_path):
+        dataset_cnn = load_dataset(
+            args.dataset_path,
+            '3.0.0'
+        )
+    else:
+        dataset_cnn = load_dataset(
+            "ccdv/cnn_dailymail",
+            '3.0.0',
+        )
 
     max_batch_size = args.batch_size
 
@@ -95,19 +117,30 @@ def main(args):
     temperature = 1
     num_beams = args.num_beams
 
-    pad_id = tokenizer.encode(tokenizer.pad_token, add_special_tokens=False)[0]
-    end_id = tokenizer.encode(tokenizer.eos_token, add_special_tokens=False)[0]
+    # pad_id = tokenizer.encode(tokenizer.pad_token, add_special_tokens=False)[0]
+    # end_id = tokenizer.encode(tokenizer.eos_token, add_special_tokens=False)[0]
+    tokenizer.pad_token_id = pad_id = end_id = tokenizer.im_end_id
+    # use this prompt to make chat model do summarize
+    system_prompt = "You are a useful assistant, please directly output the corresponding summary according to the article entered by the user."
 
     if test_trt_llm:
         config_path = os.path.join(args.engine_dir, 'config.json')
         with open(config_path, 'r') as f:
             config = json.load(f)
 
-        tensorrt_llm_llama = TRTLLaMA(args, config)
+        tensorrt_llm_qwen = TRT_QWen(args, config)
 
     if test_hf:
         profiler.start('load HF model')
-        model = AutoModelForCausalLM.from_pretrained(hf_model_location)
+        model = AutoModelForCausalLM.from_pretrained(
+            hf_model_location,
+            device_map='auto',
+            trust_remote_code=True,
+        )
+        model.generation_config = GenerationConfig.from_pretrained(
+            hf_model_location,
+            trust_remote_code=True
+        )
         profiler.stop('load HF model')
         tensorrt_llm.logger.info(
             f'Load HF model takes: {profiler.elapsed_time_in_sec("load HF model")} sec'
@@ -118,7 +151,7 @@ def main(args):
 
     def summarize_tensorrt_llm(datapoint):
         batch_size = len(datapoint['article'])
-
+        assert batch_size > 0
         line = copy.copy(datapoint['article'])
         line_encoded = []
         input_lengths = []
@@ -127,9 +160,16 @@ def main(args):
 
             line[i] = line[i].strip()
             line[i] = line[i].replace(" n't", "n't")
-
-            input_id = tokenizer.encode(line[i],
-                                        return_tensors='pt').type(torch.int32)
+            # use make_content to generate prompt
+            _, input_id_list = make_context(
+                tokenizer=tokenizer,
+                query=line[i],
+                history=[],
+                system=system_prompt,
+            )
+            input_id = torch.from_numpy(
+                np.array(input_id_list, dtype=np.int32)
+            ).type(torch.int32).unsqueeze(0)
             input_id = input_id[:, -test_token_num:]
 
             line_encoded.append(input_id)
@@ -137,7 +177,7 @@ def main(args):
 
         # do padding, should move outside the profiling to prevent the overhead
         max_length = max(input_lengths)
-        if tensorrt_llm_llama.remove_input_padding:
+        if tensorrt_llm_qwen.remove_input_padding:
             line_encoded = [torch.IntTensor(t).cuda() for t in line_encoded]
         else:
             # do padding, should move outside the profiling to prevent the overhead
@@ -156,15 +196,15 @@ def main(args):
             end_id=end_id, pad_id=pad_id, top_k=top_k, num_beams=num_beams)
 
         with torch.no_grad():
-            tensorrt_llm_llama.setup(batch_size,
+            tensorrt_llm_qwen.setup(batch_size,
                                      max_input_length=max_length,
                                      max_new_tokens=output_len)
 
-            if tensorrt_llm_llama.remove_input_padding:
-                output_ids = tensorrt_llm_llama.decode_batch(
+            if tensorrt_llm_qwen.remove_input_padding:
+                output_ids, end_step = tensorrt_llm_qwen.decode_batch(
                     line_encoded, sampling_config)
             else:
-                output_ids = tensorrt_llm_llama.decode(
+                output_ids, end_step = tensorrt_llm_qwen.decode(
                     line_encoded,
                     input_lengths,
                     sampling_config,
@@ -174,52 +214,90 @@ def main(args):
 
         # Extract a list of tensors of shape beam_width x output_ids.
         output_beams_list = [
-            tokenizer.batch_decode(output_ids[batch_idx, :,
-                                              input_lengths[batch_idx]:],
-                                   skip_special_tokens=True)
+            tokenizer.batch_decode(
+                output_ids[
+                    batch_idx,
+                    :,
+                    input_lengths[batch_idx]:input_lengths[batch_idx] + end_step
+                ],
+                skip_special_tokens=True
+            )
             for batch_idx in range(batch_size)
         ]
-        return output_beams_list, output_ids[:, :, max_length:].tolist()
+        return (
+            output_beams_list,
+            output_ids[:, :, max_length: max_length + end_step].tolist()
+        )
 
     def summarize_hf(datapoint):
         batch_size = len(datapoint['article'])
+        assert batch_size > 0
         if batch_size > 1:
             logger.warning(
                 f"HF does not support batch_size > 1 to verify correctness due to padding. Current batch size is {batch_size}"
             )
 
         line = copy.copy(datapoint['article'])
-        for i in range(batch_size):
-            line[i] = line[i] + ' TL;DR: '
+        
+        new_line_list = []
+        if batch_size > 1:
+            for i in range(batch_size):
+                line[i] = line[i] + ' TL;DR: '
 
-            line[i] = line[i].strip()
-            line[i] = line[i].replace(" n't", "n't")
-
-        line_encoded = tokenizer(line,
-                                 return_tensors='pt',
-                                 padding=True,
-                                 truncation=True)["input_ids"].type(torch.int64)
+                line[i] = line[i].strip()
+                line[i] = line[i].replace(" n't", "n't")
+                # use make_content to generate prompt
+                raw_text, _ = make_context(
+                    tokenizer=tokenizer,
+                    query=line[i],
+                    history=[],
+                    system=system_prompt
+                )
+                new_line_list.append(raw_text)
+            line_encoded = tokenizer(
+                new_line_list,
+                return_tensors='pt',
+                padding=True,
+                truncation=True,
+            )["input_ids"].type(torch.int64)
+        else:
+            line[0] = line[0] + ' TL;DR: '
+            line[0] = line[0].strip()
+            line[0] = line[0].replace(" n't", "n't")
+            # use make_content to generate prompt
+            _, input_id_list = make_context(
+                tokenizer=tokenizer,
+                query=line[0],
+                history=[],
+                system=system_prompt
+            )
+            line_encoded = torch.from_numpy(
+                np.array(input_id_list, dtype=np.int32)
+            ).type(torch.int32).unsqueeze(0)
 
         line_encoded = line_encoded[:, -test_token_num:]
         line_encoded = line_encoded.cuda()
 
         with torch.no_grad():
-            output = model.generate(line_encoded,
-                                    max_length=len(line_encoded[0]) +
-                                    output_len,
-                                    top_k=top_k,
-                                    temperature=temperature,
-                                    eos_token_id=tokenizer.eos_token_id,
-                                    pad_token_id=tokenizer.pad_token_id,
-                                    num_beams=num_beams,
-                                    num_return_sequences=num_beams,
-                                    early_stopping=True)
+            output = model.generate(
+                line_encoded,
+                max_new_tokens=len(line_encoded[0]) + output_len,
+                top_k=top_k,
+                temperature=temperature,
+                eos_token_id=tokenizer.im_end_id,
+                pad_token_id=tokenizer.im_end_id,
+                num_beams=num_beams,
+                num_return_sequences=num_beams,
+                early_stopping=True
+            )
 
-        tokens_list = output[:, len(line_encoded[0]):].tolist()
+        tokens_list = output[:, len(line_encoded[0]): len(line_encoded[0]) + output_len].tolist()
         output = output.reshape([batch_size, num_beams, -1])
         output_lines_list = [
-            tokenizer.batch_decode(output[:, i, len(line_encoded[0]):],
-                                   skip_special_tokens=True)
+            tokenizer.batch_decode(
+                output[:, i, len(line_encoded[0]): len(line_encoded[0]) + output_len],
+                skip_special_tokens=True
+            )
             for i in range(num_beams)
         ]
 
@@ -248,8 +326,8 @@ def main(args):
         logger.info(f"\n Summary : {summary}")
         logger.info("---------------------------------------------------------")
 
-    metric_tensorrt_llm = [load_metric("rouge") for _ in range(num_beams)]
-    metric_hf = [load_metric("rouge") for _ in range(num_beams)]
+    metric_tensorrt_llm = [load("rouge") for _ in range(num_beams)]
+    metric_hf = [load("rouge") for _ in range(num_beams)]
     for i in range(num_beams):
         metric_tensorrt_llm[i].seed = 0
         metric_hf[i].seed = 0
@@ -315,12 +393,11 @@ def main(args):
                     beam_idx].compute()
                 for key in computed_metrics_tensorrt_llm.keys():
                     logger.info(
-                        f'  {key} : {computed_metrics_tensorrt_llm[key].mid[2]*100}'
+                        f'  {key} : {computed_metrics_tensorrt_llm[key] * 100}'
                     )
 
                 if args.check_accuracy and beam_idx == 0:
-                    assert computed_metrics_tensorrt_llm['rouge1'].mid[
-                        2] * 100 > args.tensorrt_llm_rouge1_threshold
+                    assert computed_metrics_tensorrt_llm['rouge1'] * 100 > args.tensorrt_llm_rouge1_threshold
         if test_hf:
             np.random.seed(0)  # rouge score use sampling to compute the score
             logger.info(
@@ -331,23 +408,42 @@ def main(args):
                 computed_metrics_hf = metric_hf[beam_idx].compute()
                 for key in computed_metrics_hf.keys():
                     logger.info(
-                        f'  {key} : {computed_metrics_hf[key].mid[2]*100}')
+                        f'{key} : {computed_metrics_hf[key] * 100}'
+                    )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hf_model_location',
-                        type=str,
-                        default='/workspace/models/llama-models/llama-7b-hf')
-    parser.add_argument('--test_hf', action='store_true')
-    parser.add_argument('--test_trt_llm', action='store_true')
+    parser.add_argument(
+        '--hf_model_location',
+        type=str,
+        default=os.path.join(now_dir, "qwen_7b_chat")
+    )
+    parser.add_argument(
+        '--test_hf',
+        # action='store_true',
+        default=True,
+    )
+    parser.add_argument(
+        '--test_trt_llm',
+        action='store_true'
+        # default=True,
+    )
     parser.add_argument('--data_type',
                         type=str,
                         choices=['fp32', 'fp16'],
                         default='fp16')
-    parser.add_argument('--dataset_path', type=str, default='')
+    parser.add_argument(
+        '--dataset_path',
+        type=str,
+        default=os.path.join(now_dir, "ccdv___cnn_dailymail")
+    )
     parser.add_argument('--log_level', type=str, default='info')
-    parser.add_argument('--engine_dir', type=str, default='llama_outputs')
+    parser.add_argument(
+        '--engine_dir',
+        type=str,
+        default=os.path.join(now_dir, "trt_engines", "fp16", "1-gpu")
+    )
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--max_ite', type=int, default=20)
     parser.add_argument('--check_accuracy', action='store_true')
---
+++ tensorrt_llm_july-release-v1/examples/qwen/utils.py
@@ -0,0 +1,110 @@
+from transformers import PreTrainedTokenizer
+from typing import List, Tuple
+
+
+def make_context(
+    tokenizer: PreTrainedTokenizer,
+    query: str,
+    history: List[Tuple[str, str]] = None,
+    system: str = "",
+    max_window_size: int = 6144,
+    chat_format: str = "chatml",
+):
+    if history is None:
+        history = []
+
+    if chat_format == "chatml":
+        im_start, im_end = "<|im_start|>", "<|im_end|>"
+        im_start_tokens = [tokenizer.im_start_id]
+        im_end_tokens = [tokenizer.im_end_id]
+        nl_tokens = tokenizer.encode("\n")
+
+        def _tokenize_str(role, content):
+            return f"{role}\n{content}", tokenizer.encode(
+                role, allowed_special=set()
+            ) + nl_tokens + tokenizer.encode(content, allowed_special=set())
+
+        system_text, system_tokens_part = _tokenize_str("system", system)
+        system_tokens = im_start_tokens + system_tokens_part + im_end_tokens
+
+        raw_text = ""
+        context_tokens = []
+
+        for turn_query, turn_response in reversed(history):
+            query_text, query_tokens_part = _tokenize_str("user", turn_query)
+            query_tokens = im_start_tokens + query_tokens_part + im_end_tokens
+            response_text, response_tokens_part = _tokenize_str(
+                "assistant", turn_response
+            )
+            response_tokens = im_start_tokens + response_tokens_part + im_end_tokens
+
+            next_context_tokens = nl_tokens + query_tokens + nl_tokens + response_tokens
+            prev_chat = (
+                f"\n{im_start}{query_text}{im_end}\n{im_start}{response_text}{im_end}"
+            )
+
+            current_context_size = (
+                len(system_tokens) + len(next_context_tokens) + len(context_tokens)
+            )
+            if current_context_size < max_window_size:
+                context_tokens = next_context_tokens + context_tokens
+                raw_text = prev_chat + raw_text
+            else:
+                break
+
+        context_tokens = system_tokens + context_tokens
+        raw_text = f"{im_start}{system_text}{im_end}" + raw_text
+        context_tokens += (
+            nl_tokens
+            + im_start_tokens
+            + _tokenize_str("user", query)[1]
+            + im_end_tokens
+            + nl_tokens
+            + im_start_tokens
+            + tokenizer.encode("assistant")
+            + nl_tokens
+        )
+        raw_text += f"\n{im_start}user\n{query}{im_end}\n{im_start}assistant\n"
+
+    elif chat_format == "raw":
+        raw_text = query
+        context_tokens = tokenizer.encode(raw_text)
+    else:
+        raise NotImplementedError(f"Unknown chat format {chat_format!r}")
+
+    return raw_text, context_tokens
+  
+
+def _decode_chatml(
+    tokens: List[int],
+    stop_words: List[str],
+    eod_token_ids: List[int],
+    tokenizer: PreTrainedTokenizer,
+    raw_text_len: int,
+    context_length: int,
+    verbose: bool = False,
+    return_end_reason: bool = False,
+    errors: str='replace'
+):
+    end_reason = f"Gen length {len(tokens)}"
+    eod_token_idx = context_length
+    for eod_token_idx in range(context_length, len(tokens)):
+        if tokens[eod_token_idx] in eod_token_ids:
+            end_reason = f"Gen {tokenizer.decode([tokens[eod_token_idx]])!r}"
+            break
+
+    trim_decode_tokens = tokenizer.decode(tokens[:eod_token_idx], errors=errors)[raw_text_len:]
+    if verbose:
+        print("\nRaw Generate w/o EOD:", tokenizer.decode(tokens, errors=errors)[raw_text_len:])
+        print("\nRaw Generate:", trim_decode_tokens)
+        print("\nEnd Reason:", end_reason)
+    for stop_word in stop_words:
+        trim_decode_tokens = trim_decode_tokens.replace(stop_word, "").strip()
+    trim_decode_tokens = trim_decode_tokens.strip()
+    if verbose:
+        print("\nGenerate:", trim_decode_tokens)
+
+    if return_end_reason:
+        return trim_decode_tokens, end_reason
+    else:
+        return trim_decode_tokens
