7234d44ac0c95bf1726f54c38b76d02fce15cb87
zhaohb <zhaohbcloud@126.com>
2024-01-10 09:21:04 +0000

    visual use fp32 precision default

--- examples/qwen-vl/vit_onnx_trt.py
+++ examples/qwen-vl/vit_onnx_trt.py
@@ -11,6 +11,20 @@ import tensorrt as trt
 import argparse
 
 from tensorrt_llm._utils import str_dtype_to_torch
+
+import tensorrt as trt
+from itertools import tee
+
+from polygraphy.backend.trt import (
+    network_from_onnx_path,
+    engine_from_network,
+    save_engine,
+    Profile,
+)
+
+from polygraphy.backend.trt import CreateConfig
+from tensorrt import MemoryPoolType
+
 class Preprocss:
     def __init__(self,
                  image_size:int,
@@ -38,18 +52,19 @@ class Preprocss:
             images.append(self.image_transform(image))
         images = torch.stack(images, dim=0)
         return images
+    
 class ONNX_TRT:
     def __init__(self,image_size):
         self.image_size = image_size
     def export_onnx(self,onnx_file_path,pretrained_model_path):
         
         image_pre_obj = Preprocss(self.image_size)
-        torch_dtype = str_dtype_to_torch("float16")
+        torch_dtype = str_dtype_to_torch("float32")
         model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_path,
             device_map="cpu",
             torch_dtype=torch_dtype,
-            # fp16=True,
+            fp32=True,
             trust_remote_code=True
         ).eval()
         image_url = ['https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg']
@@ -58,7 +73,8 @@ class ONNX_TRT:
         if not os.path.exists('./input_pt'):
             os.mkdir('./input_pt')
             torch.save(image, './input_pt/image.pt')
-        model_visual = model.transformer.visual.to(device).to(torch_dtype)
+        #model_visual = model.transformer.visual.to(device).to(torch_dtype)
+        model_visual = model.transformer.visual
         model_visual.eval()
         
         torch.onnx.export(model_visual,
@@ -71,50 +87,79 @@ class ONNX_TRT:
                             'input':{0:'batch'}
                         }
                         )
-    def generate_trt_engine(self,onnxFile,planFile,minBS=1,optBS=2,maxBS=4):
+    def generate_trt_engine(self,onnxFile,planFile,minBS=1,optBS=2,maxBS=4,use_polygraph=False):
         import tensorrt as trt
         from time import time
-        logger = trt.Logger(trt.Logger.INFO)
-        builder = trt.Builder(logger)
-        network = builder.create_network(
-            1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
-        profile = builder.create_optimization_profile()
-        config = builder.create_builder_config()
-        config.set_flag(trt.BuilderFlag.FP16)
 
-        parser = trt.OnnxParser(network, logger)
-        print("======onnxFile",onnxFile)
+        if use_polygraph:
+            print("we are using polygraph tools get engine file !!!")
+            #preview_features = [trt.PreviewFeature.FASTER_DYNAMIC_SHAPES_0805]
+            preview_features = []
+            
+            profiles = [Profile().add(
+                "input",
+                min=(minBS, 3, self.image_size, self.image_size ),
+                opt=(optBS, 3, self.image_size, self.image_size ), # Optimized based on the inputs.
+                max=(maxBS, 3, self.image_size, self.image_size ),
+            )]
+            trt_inference_config = CreateConfig(
+                            fp16=True,
+                            memory_pool_limits = {MemoryPoolType.WORKSPACE: 2048 * 1024 * 1024},
+                            profiles=profiles,
+                            precision_constraints=("obey"),
+                            builder_optimization_level=3,
+                            preview_features=preview_features
+                        )
+            
+            onnx_network = network_from_onnx_path(onnxFile)
+            
+            trt_engine = engine_from_network(onnx_network, trt_inference_config)
+            
+            save_engine(trt_engine, planFile)
 
-        with open(onnxFile, 'rb') as model:
-            if not parser.parse(model.read(), "/".join(onnxFile.split("/"))):
-                print("Failed parsing %s" % onnxFile)
-                for error in range(parser.num_errors):
-                    print(parser.get_error(error))
-            print("Succeeded parsing %s" % onnxFile)
-        print("Begin convert onnx to TensorRT engine, need wait a few minutes")
-
-        nBS = -1
-        nMinBS = minBS
-        nOptBS = optBS
-        nMaxBS = maxBS
-        inputT = network.get_input(0)
-        inputT.shape = [nBS, 3, self.image_size, self.image_size]
-        profile.set_shape(inputT.name, [nMinBS, 3, self.image_size, self.image_size],
-                          [nOptBS, 3, self.image_size, self.image_size], [nMaxBS, 3, self.image_size, self.image_size])
-
-        config.add_optimization_profile(profile)
-
-        t0 = time()
-        engineString = builder.build_serialized_network(network, config)
-        t1 = time()
-        if engineString == None:
-            print("Failed building %s" % planFile)
         else:
-            print("Succeeded building %s in %d s" % (planFile, t1 - t0))
-        print("plan file is",planFile)
-        with open(planFile, 'wb') as f:
-            f.write(engineString)
+            logger = trt.Logger(trt.Logger.INFO)
+            builder = trt.Builder(logger)
+            network = builder.create_network(
+                1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+            profile = builder.create_optimization_profile()
+            config = builder.create_builder_config()
+            # breakpoint()
+            #config.set_flag(trt.BuilderFlag.FP16)
+            #config.set_flag(trt.BuilderFlag.OBEY_PRECISION_CONSTRAINTS)
 
+            parser = trt.OnnxParser(network, logger)
+            print("======onnxFile",onnxFile)
+
+            with open(onnxFile, 'rb') as model:
+                if not parser.parse(model.read(), "/".join(onnxFile.split("/"))):
+                    print("Failed parsing %s" % onnxFile)
+                    for error in range(parser.num_errors):
+                        print(parser.get_error(error))
+                print("Succeeded parsing %s" % onnxFile)
+            print("Begin convert onnx to TensorRT engine, need wait a few minutes")
+
+            nBS = -1
+            nMinBS = minBS
+            nOptBS = optBS
+            nMaxBS = maxBS
+            inputT = network.get_input(0)
+            inputT.shape = [nBS, 3, self.image_size, self.image_size]
+            profile.set_shape(inputT.name, [nMinBS, 3, self.image_size, self.image_size],
+                              [nOptBS, 3, self.image_size, self.image_size], [nMaxBS, 3, self.image_size, self.image_size])
+
+            config.add_optimization_profile(profile)
+
+            t0 = time()
+            engineString = builder.build_serialized_network(network, config)
+            t1 = time()
+            if engineString == None:
+                print("Failed building %s" % planFile)
+            else:
+                print("Succeeded building %s in %d s" % (planFile, t1 - t0))
+            print("plan file is",planFile)
+            with open(planFile, 'wb') as f:
+                f.write(engineString)
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -125,6 +170,7 @@ def parse_arguments():
     parser.add_argument('--minBS',type=int, default=1)
     parser.add_argument('--optBS',type=int, default=1)
     parser.add_argument('--maxBS',type=int, default=4)
+    parser.add_argument('--use_polygraph',type=bool, default=False)
     args = parser.parse_args()
     return args
     
@@ -144,10 +190,10 @@ if __name__ == '__main__':
         onnx_trt_obj = ONNX_TRT(config.visual['image_size'])
     
     if args.only_trt:
-        onnx_trt_obj.generate_trt_engine(args.onnxFile,args.planFile,args.minBS,args.optBS,args.maxBS)
+        onnx_trt_obj.generate_trt_engine(args.onnxFile,args.planFile,args.minBS,args.optBS,args.maxBS,args.use_polygraph)
     else:
         onnx_trt_obj.export_onnx(args.onnxFile,args.pretrained_model_path)
-        onnx_trt_obj.generate_trt_engine(args.onnxFile,args.planFile,args.minBS,args.optBS,args.maxBS)
+        onnx_trt_obj.generate_trt_engine(args.onnxFile,args.planFile,args.minBS,args.optBS,args.maxBS,args.use_polygraph)
         
     
         
